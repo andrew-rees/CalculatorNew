3e0ad85ef08881887a3885a0c1b08b386716024b
Andrew Rees <andrewtrevor.rees@gmail.com>
2018-06-01 12:37:16 +0100

    simple 2 number calc

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const readline = require('readline-sync');
+
 console.log("Welcome to the New Calculator");
+
+console.log("Please enter your first number");
+const firstNumberInput = readline.prompt();
+const firstNumber = +firstNumberInput
+
+console.log("Please enter your second number");
+const secondNumberInput = readline.prompt();
+const secondNumber = +secondNumberInput
+
+console.log("Please enter your operator (eg +, -, /, *");
+const operatorInput = readline.prompt();
+
+switch (operatorInput) {
+    case "+":
+    console.log ("The answer is: " + firstNumber + secondNumber)
+    break;
+    case "-":
+    console.log ("The answer is: " + firstNumber - secondNumber)
+    break;
+    case "*":
+    console.log ("The answer is: " + firstNumber * secondNumber)
+    break;
+    case "/":
+    console.log ("The answer is: " + firstNumber / secondNumber)
+    break;
+}
